e903a5a8c90a79844d88a0139128437cd287e9fe
ElectroZuluMoon <danizuluaga2004@gmail.com>
2025-11-24 23:11:40 -0500

    Se crearon las funciones de cada pagina

--- app.py
+++ app.py
@@ -8,10 +8,44 @@ RUTA_EXCEL = "data/Accidentes_50.xlsx"
 
 @st.cache_data
 def cargar_datos():
-    return pd.read_excel(RUTA_EXCEL)
+    try:
+        df = pd.read_excel(RUTA_EXCEL)
+        return pd.read_excel(RUTA_EXCEL)
+    except FileNotFoundError:
+        st.error("No se encontró el archivo")
+        return pd.DataFrame()
+
 
 def guardar_datos(df):
     df.to_excel(RUTA_EXCEL, index=False)
+
+def pagina_inicio():
+    st.title("Sistema de Accidentes Viales ")
+    st.image(
+        "https://media.istockphoto.com/id/466327320/es/foto/car-crash-colisi%C3%B3n-en-urban-street.jpg?s=612x612&w=0&k=20&c=jVNAuAW6hOGeD07CoS5jiFg5QdqbOGQm5d6o3fOrnhg=",
+        use_container_width=True)
+
+def pagina_registrar(df):
+    st.title("Registrar Accidente")
+    st.info("Formulario")
+
+def pagina_consultar_modificar(df):
+    st.title("Consultar / Modificar Accidente")
+    st.info("Busqueda y edicion de Accidentes")
+
+def pagina_filtros(df):
+    st.title("Filtros")
+    st.info("Filtros de Accidentes")
+
+def pagina_graficos(df):
+    st.title("Graficos")
+    st.info("Graficos segun los filtros")
+
+def estadisticas(df):
+    st.title("Estadisticas")
+    st.info("Estadisticas avanzadas")
+
+    
 
 
 st.sidebar.title("Menu")
